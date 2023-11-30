c8a34a64ff07e7f1f5fdcab5a2ea575308c370a9
Sanjeeban <sanjeeban5644@gmail.com>
2023-11-30 20:56:46 +0530

    Release Version 2.0.0
    The App is updated according to the WorkFlow diagram of the project.

--- app/src/main/java/com/example/finalproject/Menu/MenuActivity.java
+++ app/src/main/java/com/example/finalproject/Menu/MenuActivity.java
@@ -4,18 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.finalproject.Level1.MatchGame;
+import com.example.finalproject.Level1.Menu_Level1;
 import com.example.finalproject.Level2.Animal_Quiz;
+import com.example.finalproject.Level2.Menu_Level2;
+import com.example.finalproject.Level3.Menu_Level3;
 import com.example.finalproject.R;
 import com.example.finalproject.login.LoginPage;
 
 public class MenuActivity extends AppCompatActivity {
 
-    Button level1_button,level2_button;
+    Button level1_button,level2_button,level3_button;
 
 
 
@@ -31,23 +35,35 @@ public class MenuActivity extends AppCompatActivity {
 
         level1_button = findViewById(R.id.level1);
         level2_button = findViewById(R.id.level2);
+        level3_button = findViewById(R.id.level3);
 
         level1_button.setOnClickListener(view -> {
-            Intent intent = new Intent(MenuActivity.this, MatchGame.class);
+            Intent intent = new Intent(MenuActivity.this, Menu_Level1.class);
             startActivity(intent);
         });
 
         level2_button.setOnClickListener(view -> {
-            Intent intent = new Intent(MenuActivity.this, Animal_Quiz.class);
+            Intent intent = new Intent(MenuActivity.this, Menu_Level2.class);
             startActivity(intent);
         });
 
+        level3_button.setOnClickListener(view -> {
+            Intent intent = new Intent(MenuActivity.this, Menu_Level3.class);
+            startActivity(intent);
+        });
+
+        Button exitApp = findViewById(R.id.exitApp);
+        exitApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exitApplication(); // This will close the current activity and exit the app
+            }
+        });
 
 
+    }
 
-
-
-
-
+    private void exitApplication() {
+        finishAffinity(); // This will close all activities and exit the app
     }
 }
